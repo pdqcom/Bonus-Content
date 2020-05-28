841bd4876e2b790208bd29d30b13802a5163c557
Jordan <52014208+BaronCupboards@users.noreply.github.com>
2020-05-28 10:52:39 -0600

    Create UninstallAppxPackages.ps1

---
+++ Appx/UninstallAppxPackages.ps1
@@ -0,0 +1,37 @@
+##Get appx Packages
+$Packages = Get-AppxPackage
+
+##Create Your Whitelist
+$Whitelist = @(
+    '*WindowsCalculator*',
+    '*MSPaint*',
+    '*Office.OneNote*',
+    '*Microsoft.net*',
+    '*MicrosoftEdge*',
+    '*WindowsStore*'
+)
+
+###Get All Dependencies
+ForEach($Dependency in $Whitelist){
+    (Get-AppxPackage -AllUsers -Name "$Dependency").dependencies | Foreach{
+        $NewAdd = "*" + $_.Name + "*"
+        if($_.name -ne $null -and $Whitelist -notcontains $NewAdd){
+            $Whitelist += $NewAdd
+       }
+    }
+}
+
+##View all applications not in your whitelist
+ForEach($App in $Packages){
+    $Matched = $false
+    Foreach($Item in $Whitelist){
+        If($App -like $Item){
+            $Matched = $true
+            break
+        }
+    }
+    ###Nonremovable attribute does not exist before 1809, so if you are running this on an earlier build remove "-and $app.NonRemovable -eq $false" rt it will attempt to remove everything
+    if($matched -eq $false -and $app.NonRemovable -eq $false){
+        Remove-AppxPackage -AllUsers -Package $app.PackageFullName -ErrorAction SilentlyContinue
+    }
+}
